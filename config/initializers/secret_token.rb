e53565a6135411465f7b3b1e828fd73467fdc78b
Keith Barto <kbarto@immersiveps.com>
2013-11-16 13:18:01 -0700

    Immersive

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Immersive::Application.config.secret_key_base = 'a9876d89d53444f73fef81dd6f4bd14fc6d7408c80e12c898858a4c10304393dd04239c7c194135b203e658860918a88ebe8b27d9eaa385d774f0b160e5827dc'
